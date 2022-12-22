dc69c42de407df2089ff9548355ef0808d5fb06b
Opu58 <opu260993@gmail.com>
2022-12-22 09:57:09 +0600

    update

---
+++ js/index.js
@@ -0,0 +1,168 @@
+// var a = 10;
+// var y = 12.5;
+// var abc = "Mama";
+// document.write("a: " + a);
+// console.log("y: " + y);
+// document.write("<p style ='color: Green;'> This is a Paragraph. </p>");
+// document.write(" This is second Paragraph");
+// document.write("<br>");
+// document.write("This is Third Paragraph");
+// document.write("This is Fourth Paragraph");
+// document.write("<p style ='color: Blue;'> This is  Second Paragraph. </p>");
+// console.log(y.toString());
+// var h = "this is string";
+// h.parseInt();
+
+//  user input
+//  var FirstName = prompt("Enter Your First Name");
+//  var LastName = prompt("Enter Your Last Name");
+//  document.write(FirstName + LastName);
+
+// var FirstNumber = prompt("Enter Your First Number");
+// var LastNumber = prompt("Enter Your Last Number");
+// document.write(Number(FirstNumber) + Number(LastNumber));
+
+// let x = "Welcome";
+// let y = "to Bangladesh";
+
+// document.write (x.concat (y));
+
+// // function for string length - .length
+// document.write("Length of Last Name " + studentLastName.length + "</br>" );
+
+// let x = "300";
+// let y = "200";
+// document.write(Number(x) + Number(y));
+
+// let x = "300";
+// let y = "200";
+// document.write(Number(x) - Number(y));
+
+// var x = parsefloat (prompt("Enter Your First Number"));
+//  var y = parsefloat (prompt("Enter Your Last Number");
+// Document.write('Sum: ', x + y + '<br>');
+// Document.write('Sub: ', x - y + '<br>');
+// Document.write('Product: ', x * y + '<br>');
+// Document.write('Divide: ', x / y + '<br>');
+// Document.write('Modulas: ', x % y + '<br>');
+
+// // conditional statement
+// var digit =(Number (prompt("Enter Your First Number")));
+// if (digit < 100)
+// document.write("< 100");
+// else if ( digit > 100)
+// document.write("> 100");
+// else if (digit > 200)
+// document.write("> 200");
+
+// // conditional statement
+// var a =(Number (prompt("Enter Your First Number")));
+// if (digit < 100)
+// document.write("< 100");
+// else if ( digit > 100 && digit <= 200)
+// document.write("> 100 & <= 200");
+// else if (digit > 200 && digit <= 300)
+// document.write("> 200 & <= 300");
+
+// else
+// document.write("Invalid");
+
+// // Ternary operator
+// // if (digit > 0)
+// // "<br/> Positive <br/>"
+// // else
+// // "<br/> Negative <br/>"
+// var digit = (Number(prompt("Enter Any Number")));
+// var print = digit > 0 ? "<br/> Positive <br/>" : "<br/> Negative <br/>";
+// document.write(print);
+//
+
+// switch case
+
+// var digit = prompt("Enter any number");
+// var t = parseInt(digit / 10);
+// switch (t) {
+//   case 10:
+//   case 9:
+//   case 8:
+//     document.write("<br/> A+");
+//     break;
+//  case 7:
+//     document.write("<br/> A");
+//     break;
+//     case 6:
+//     document.write("<br/> B");
+//     break; 
+//     default:
+//         document.write("<br/> Invalid")
+
+// }
+
+
+
+// var ShowLetter = prompt("Enter String").toUpperCase();
+
+// switch(ShowLetter) {
+
+//   case 'A': document.write("<br/> A has been pressed");
+//     break;
+
+//  case 'B': document.write("<br/> B has been pressed);  
+//     break;
+//     case 'C': document.write("<br/> C has been pressed"); 
+//         break; 
+ 
+
+// }
+
+
+// var i = 1;
+// for (  var i = 0; i <= 10; i++)
+// {
+// document.write(i + '<br/>')
+
+// }
+
+
+// var i = 1;
+// while (i <= 10)
+// {
+//     document.write(i + '<br/>');
+//     i++;    
+// }
+// do {
+//     document.write(i+"<br>");
+//     i++;
+// }
+// while(i<= 12)
+
+
+
+// Without function
+// var num = 3;
+// document.write("<br> Result is: " +num * num);
+
+// var num = 4;
+// document.write("<br> Result is: " +num * num);
+
+// var num = 5;
+// document.write("<br> Result is: " +num * num);
+
+
+// Using function
+// function square(){
+//     var num = 6;
+//     document.write("<br/> Result is: " +num * num);
+// }
+// square();
+
+
+
+// Using function
+function square(){
+    var num1 = 2;
+    var num2 = 3;
+    var num3 = 4;
+    document.write("<br/> Result is: " +num * num);
+}
+square();
